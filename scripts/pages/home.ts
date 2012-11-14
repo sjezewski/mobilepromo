426dbb6bb7fd8823a4b335214c1b2a2abd6d88cf
smoock <stephen.moock@moovweb.com>
2012-11-13 19:53:25 -0800

    function to insert banner

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,13 +1,6 @@
 $("./body") {
 	add_class("mw_home")
 	
-	$(".//div[@class='mw_mobile_menu']") {
-	  insert_after("div", class: "mw_banner"){
-  		# move promo banner here
-  		move_here("//div[@id='mobile_banner']", "bottom"){
-  		attribute("style", "")
-  		}
-  	}
-	}
+	
 	
 }
