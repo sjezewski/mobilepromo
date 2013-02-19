c178fa23e140c123ceb3593c6ec5fe79f7c3ed03
Naseem Makiya <naseem.makiya@moovweb.com>
2013-02-19 15:13:05 -0800

    Revert "Header toggler added"
    This reverts commit e98af0dfede59eeb19a5f52de83467cd28874fa2.

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,6 +1,5 @@
 $('./body') {
   $(".//div[@id='Header']") {
-    attribute("data-ur-set", "toggler")
       # Move top menu into #Logo container
       $("./div[@id='Logo']") {
       move_here("../../../div[@id='TopMenu']", "bottom"){
@@ -14,9 +13,6 @@ $('./body') {
 
       # Inject header template
       inject_after(read("../sections/header.html"))
-      $("..//div[@class='mw_menu_btn']") {
-        attribute("data-ur-toggler-component", "button")
-      }
 
       # Fill search template
       $("..//div[@class='mw_search']"){
@@ -45,12 +41,7 @@ $('./body') {
       remove()
     }
 
-    # Move menu items into #Header
-    move_here("../div[@id='Menu']", "bottom"){
-      # Declare toggler content element which is enabled/diabled by toggler button
-      attribute("data-ur-toggler-component", "content")
-      # Move "Category List" element on bottom of #Menu element
-      move_here("../../div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
-    }
+    # Remove header links temporarily
+    remove("../div[@id='Menu']")
   }
 }
