62027dc22b89078eb52619e674408949fca09b5b
Sean Jezewski <sean@moovweb.com>
2013-06-14 10:19:16 -0700

    Cleanup

--- scripts/pages/promo.ts
+++ scripts/pages/promo.ts
@@ -3,13 +3,12 @@ log("PROMO")
 # Relative links need the magic prefix
 
 $$(".mw_relative") {
+  # TODO: Figure out automated way to detect relative links so I don't need to specify them
+
   attribute("href") {
     value(magic_passthrough(value()))
   }
   attribute("action") {
     value(magic_passthrough(value()))
-#    value("//" + $host + "/__promo__" + value())
   }
-
-
 }
