6ca448205e1ad64533ce7bb546a98568633b2852
Sean Jezewski <sean@moovweb.com>
2013-06-14 10:47:39 -0700

    Style photo upload page

--- scripts/pages/promo.ts
+++ scripts/pages/promo.ts
@@ -1,5 +1,22 @@
 log("PROMO")
 
+$("/html/body") {
+  add_class("mw_promo")
+
+  match($path) {
+    with(/submission\/new/) {
+      add_class("submission")
+    }
+    with(/survey\/new/) {
+      add_class("survey")
+    }
+    with(/promo\/complete/) {
+      add_class("promo")
+    }
+  }
+
+}
+
 # Relative links need the magic prefix
 
 $$(".mw_relative") {
